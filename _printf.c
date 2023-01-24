37d97ec5011276110fb932afcbc5830eaaddeeff
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-24 16:07:54 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -17,15 +17,13 @@ int _printf(const char *format, ...)
 	int i, count = 0;
 	char c, *s;
 
-	va_start(args, format);
-
 	if (format == NULL)
 		return (-1);
 	if (*format == 0)
 		return (0);
 	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
-
+	va_start(args, format);
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
