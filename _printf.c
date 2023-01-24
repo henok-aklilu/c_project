f0beb4b172a1fff79eb0c5d92dcd26246bdacb89
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-24 14:02:49 +0300

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, count = 0;
+	char c, *s;
+
+	va_start(args, format);
+
+	if (format == NULL || *format == 0)
+		return (-1);
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				write(1, s, strlen(s) + 1);
+				count += strlen(s);
+			}
+			else
+			{
+				write(1, &format[i], 1);
+				count++;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
